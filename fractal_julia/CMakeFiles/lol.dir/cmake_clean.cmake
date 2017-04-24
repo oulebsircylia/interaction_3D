0c39782a92ed6b2695b72d319ac65b7c318b9624
oulebsircylia <cyliaoulebsir@yahoofr>
2017-04-25 00:16:06 +0200

    fractal de julia

---
+++ fractal_julia/CMakeFiles/lol.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lol.dir/lol.cpp.o"
+  "lol.pdb"
+  "lol"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/lol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
